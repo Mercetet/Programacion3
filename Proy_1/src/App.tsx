c50d5c772f7b3a409adcd514c9c6095c989a7dc3
Merce <mercetetilla@gmail.com>
2024-09-25 16:14:14 -0300

    Primer proyecto react

---
+++ Proy_1/src/App.tsx
@@ -0,0 +1,24 @@
+import { MiPrimerComponent } from "./components/MiPrimerComponent/MiPrimerComponent"
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect"
+import { FormComponent } from "./components/FormComponent/FormComponent"
+import { AppProduct } from "./components/AppProduct/AppProduct"
+
+
+
+export const App = () => {
+
+  return (
+    <div style={{display:'flex', flexDirection:'column', gap:'2vw'}}>
+        {/* <MiPrimerComponent 
+            texto="Texto desde propiedades" 
+            color="red" 
+            fontSize={5} 
+        />
+        <ComponentCounter />
+        <ComponentUseEffect />
+        <FormComponent /> */}
+        <AppProduct />
+    </div>
+  )
+}
